908e52e5877772c00872008a41d79d8a19b204f2
igna-zanella <igna.zanella@gmail.com>
2023-09-03 17:52:59 -0300

    instalación de prompt-sync

--- suma.js
+++ suma.js
@@ -4,7 +4,7 @@ var numeroDos;
 numeroUno = prompt('Ingrese un número: ','');
 numeroDos = prompt('Ingresos otro número: ','');
 var resultado;
-resultado = numeroUno * numeroDos;
+resultado = numeroUno + numeroDos;
 console.log("El resultado es: " + resultado);
 
 let sign = prompt("¿Cuál es tu signo? ");
