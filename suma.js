4e37b75aed1f522eea6616f7a4973da27b4a08bc
igna-zanella <igna.zanella@gmail.com>
2023-09-03 18:26:44 -0300

    correción en la función suma con parseInt()

--- suma.js
+++ suma.js
@@ -1,10 +1,10 @@
 const prompt = require("prompt-sync")();
 var numeroUno;
 var numeroDos;
-numeroUno = prompt('Ingrese un número: ','');
-numeroDos = prompt('Ingresos otro número: ','');
+numeroUno = prompt('Ingrese un número: ','0');
+numeroDos = prompt('Ingresos otro número: ','0');
 var resultado;
-resultado = numeroUno + numeroDos;
+resultado = parseInt(numeroUno)+parseInt(numeroDos);
 console.log("El resultado es: " + resultado);
 
 let sign = prompt("¿Cuál es tu signo? ");
