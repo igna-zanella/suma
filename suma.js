b1f211784d9efdde8bf09e572a9066bd1287e931
igna-zanella <igna.zanella@gmail.com>
2023-09-23 23:04:44 -0300

    se agrega ejemplo de método 'repeat'

--- suma.js
+++ suma.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync")()
 var numeroUno, numeroDos
 numeroUno = prompt('Ingrese un número: ','0')
-numeroDos = prompt('Ingresos otro número: ','0')
+numeroDos = prompt('Ingrese otro número: ','0')
 var resultado
 resultado = parseInt(numeroUno)+parseInt(numeroDos)
 console.log("El resultado es: " + resultado)
@@ -11,3 +11,19 @@ let sign = prompt("¿Cuál es tu signo? ")
 // if (sign == sign) {
   console.log("¡Increíble! Yo también soy de " + sign + " ¡Qué casualidad!")
 // }
+
+function admision(){
+let nombre = prompt("Ingrese su nombre: ")
+if (nombre !== "cosme fulanito" && nombre !== "Cosme Fulanito") {
+    console.log("Puede pasar")
+} else {
+    console.log("No se admiten Homeros")
+}
+}
+admision()
+
+function hacerHelados(cantidad) {
+  return '🍦'.repeat(cantidad);
+  }
+  let misHelados = hacerHelados(3);
+  console.log(misHelados); // Mostrará en consola '🍦🍦🍦'
